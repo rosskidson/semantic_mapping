aeaeec3f1da615cf9d4b56be7d78d65b485858d4
Ross Kidson <ross.kidson@gmail.com>
2013-01-28 15:03:39 +0100

    gui node displaying popups

---
+++ qtgui/src/main.cpp
@@ -0,0 +1,39 @@
+
+#include <ros/ros.h>
+#include "std_msgs/String.h"
+
+#include <QApplication>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QWidget>
+#include <QObject>
+
+//class MainWindow : public QWidget
+//{
+//    Q_OBJECT
+//public slots:
+//      void buttonPressed();
+//};
+
+//void MainWindow::buttonPressed()
+//{
+//    QMessageBox::information(0, QString("Information"), QString("You've pressed the button \"Press Me!\""), QMessageBox::Ok);
+//}
+
+void displayMessage(const std_msgs::String::ConstPtr& msg)
+{
+  QMessageBox msgBox;
+  msgBox.setText("The document has been modified.");
+  msgBox.exec();
+}
+
+int main(int argc, char** argv)
+{
+  ros::init (argc, argv, "semantic_mapping_controller");
+  ros::NodeHandle n;
+  ros::Subscriber sub = n.subscribe("ui_popup", 1000, displayMessage);
+
+  QApplication a(argc, argv);
+  ros::spin();
+  return a.exec();
+}

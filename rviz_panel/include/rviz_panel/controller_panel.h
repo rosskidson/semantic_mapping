fdd849be3091f541517623b75f5549ff5bddb98a
Ross Kidson <ross.kidson@gmail.com>
2013-02-03 21:30:19 +0100

    rviz panel plugin works, however no field

--- rviz_panel/include/rviz_panel/controller_panel.h
+++ rviz_panel/include/rviz_panel/controller_panel.h
@@ -1,93 +1,54 @@
-#ifndef TELEOP_PANEL_H
-#define TELEOP_PANEL_H
+#ifndef CONTROLLER_PANEL_H
+#define CONTROLLER_PANEL_H
 
 #include <ros/ros.h>
-
 #include <rviz/panel.h>
 
 class QLineEdit;
 
-namespace rviz_plugin_tutorials
+namespace rviz_panel
 {
 
-class DriveWidget;
+  class ControllerPanel: public rviz::Panel
+  {
+  // This class uses Qt slots and is a subclass of QObject, so it needs
+  // the Q_OBJECT macro.
+  Q_OBJECT
+  public:
+    // QWidget subclass constructors usually take a parent widget
+    // parameter (which usually defaults to 0).  At the same time,
+    // pluginlib::ClassLoader creates instances by calling the default
+    // constructor (with no arguments).  Taking the parameter and giving
+    // a default of 0 lets the default constructor work and also lets
+    // someone using the class for something else to pass in a parent
+    // widget as they normally would with Qt.
+    ControllerPanel( QWidget* parent = 0 );
 
-// BEGIN_TUTORIAL
-// Here we declare our new subclass of rviz::Panel.  Every panel which
-// can be added via the Panels/Add_New_Panel menu is a subclass of
-// rviz::Panel.
-//
-// TeleopPanel will show a text-entry field to set the output topic
-// and a 2D control area.  The 2D control area is implemented by the
-// DriveWidget class, and is described there.
-class TeleopPanel: public rviz::Panel
-{
-// This class uses Qt slots and is a subclass of QObject, so it needs
-// the Q_OBJECT macro.
-Q_OBJECT
-public:
-  // QWidget subclass constructors usually take a parent widget
-  // parameter (which usually defaults to 0).  At the same time,
-  // pluginlib::ClassLoader creates instances by calling the default
-  // constructor (with no arguments).  Taking the parameter and giving
-  // a default of 0 lets the default constructor work and also lets
-  // someone using the class for something else to pass in a parent
-  // widget as they normally would with Qt.
-  TeleopPanel( QWidget* parent = 0 );
+    // A couple of public Qt slots.
+  public Q_SLOTS:
+    // In this example setTopic() does not get connected to any signal
+    // (it is called directly), but it is easy to define it as a public
+    // slot instead of a private function in case it would be useful to
+    // some other user.
+    void setTopic( const QString& topic );
 
-  // Now we declare overrides of rviz::Panel functions for saving and
-  // loading data from the config file.  Here the data is the
-  // topic name.
-  virtual void load( const rviz::Config& config );
-  virtual void save( rviz::Config config ) const;
+    // Here we declare some internal slots.
+  protected Q_SLOTS:
+    // updateTopic() reads the topic name from the QLineEdit and calls
+    // setTopic() with the result.
+    void updateTopic();
 
-  // Next come a couple of public Qt slots.
-public Q_SLOTS:
-  // The control area, DriveWidget, sends its output to a Qt signal
-  // for ease of re-use, so here we declare a Qt slot to receive it.
-  void setVel( float linear_velocity_, float angular_velocity_ );
+  protected:
+    // One-line text editor for entering the outgoing ROS topic name.
+    QLineEdit* output_topic_editor_;
 
-  // In this example setTopic() does not get connected to any signal
-  // (it is called directly), but it is easy to define it as a public
-  // slot instead of a private function in case it would be useful to
-  // some other user.
-  void setTopic( const QString& topic );
+    // The current name of the output topic.
+    QString output_topic_;
 
-  // Here we declare some internal slots.
-protected Q_SLOTS:
-  // sendvel() publishes the current velocity values to a ROS
-  // topic.  Internally this is connected to a timer which calls it 10
-  // times per second.
-  void sendVel();
-
-  // updateTopic() reads the topic name from the QLineEdit and calls
-  // setTopic() with the result.
-  void updateTopic();
-
-  // Then we finish up with protected member variables.
-protected:
-  // The control-area widget which turns mouse events into command
-  // velocities.
-  DriveWidget* drive_widget_;
-
-  // One-line text editor for entering the outgoing ROS topic name.
-  QLineEdit* output_topic_editor_;
-
-  // The current name of the output topic.
-  QString output_topic_;
-
-  // The ROS publisher for the command velocity.
-  ros::Publisher velocity_publisher_;
-
-  // The ROS node handle.
-  ros::NodeHandle nh_;
-
-  // The latest velocity values from the drive widget.
-  float linear_velocity_;
-  float angular_velocity_;
-  // END_TUTORIAL
-};
+    // The ROS node handle.
+    ros::NodeHandle nh_;
+  };
 
 } // end namespace rviz_plugin_tutorials
 
-#endif // TELEOP_PANEL_H
+#endif // CONTROLLER_PANEL_H
